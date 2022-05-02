516b90df6e807fdd75369e9f3c0532a660469419
jtorresl79c <83847060+jtorresl79c@users.noreply.github.com>
2022-05-02 11:58:44 -0700

    Curso de MVC .Net C#| Filtros, seguridad | #3

---
+++ crudleon/Filters/VerifySession.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using crudleon.Controllers;
+using crudleon.Models.Entity.Prime;
+
+namespace crudleon.Filters
+{
+    public class VerifySession : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            user oUser = (user)HttpContext.Current.Session["User"];
+
+            if (oUser == null)
+            {
+                if (filterContext.Controller is AccessController == false) // == false esta de mas, pero para que se entienda asi se puso
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Access/Index");
+                }
+            }
+            else
+            {
+                if (filterContext.Controller is AccessController == true)
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Home");
+                }
+            }
+            
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
