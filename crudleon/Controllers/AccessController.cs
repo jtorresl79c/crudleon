f536ddb5c46200106f7316ac859d912b051c5da0
Jason Torres Luis <jtorresl79c@gmail.com>
2022-05-01 18:59:13 -0700

    Curso de MVC .Net C#| Introducción, controladores y vistas | #1

---
+++ crudleon/Controllers/AccessController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace crudleon.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(string user, string password)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurrio un error :c" + ex.Message);
+            }
+        }
+    }
+}
